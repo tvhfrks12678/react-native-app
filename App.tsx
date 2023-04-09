6587434243dd8fe3b7e47b003499d555725697de
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-04-10 06:39:17 +0900

    :sparkles: react-native-youtube-iframeでYoutubeを表示する

--- App.tsx
+++ App.tsx
@@ -5,93 +5,20 @@
  * @format
  */
 
-import React, {useRef, useState} from 'react';
-import {
-  StyleSheet,
-  View,
-  SafeAreaView,
-  Button,
-  Text,
-  Platform,
-} from 'react-native';
-import WebView, {WebViewMessageEvent} from 'react-native-webview';
-import {buildHTML} from './iFrame';
+import React, {useState, useCallback, useRef} from 'react';
+import {Button, View, Alert} from 'react-native';
+import YoutubePlayer from 'react-native-youtube-iframe';
 
-const App = () => {
-  const webRef = useRef<WebView>(null);
-  const [time, setTime] = useState('00:00');
+export default function App() {
+  // const [playing, setPlaying] = useState(false);
 
-  const playVideo = () => {
-    webRef.current?.injectJavaScript('play()');
-  };
-
-  const pauseVideo = () => {
-    webRef.current?.injectJavaScript('pause()');
-  };
-
-  const restartVideo = () => {
-    webRef.current?.injectJavaScript('seekTo(0)');
-  };
-
-  const onMessageRecieved = (message: WebViewMessageEvent) => {
-    const responseJSON = JSON.parse(message.nativeEvent.data);
-    const minutes = Math.round(responseJSON.currentDuration / 60);
-    const seconds = Math.round(responseJSON.currentDuration % 60);
-    const minutesText = minutes < 10 ? `0${minutes}` : minutes;
-    const secondsText = seconds < 10 ? `0${seconds}` : seconds;
-    setTime(`${minutesText}:${secondsText}`);
-  };
+  // const togglePlaying = useCallback(() => {
+  //   setPlaying(prev => !prev);
+  // }, []);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.videoContainer}>
-        <WebView
-          ref={webRef}
-          source={{html: buildHTML('hQJeGBYI3C8')}}
-          allowsFullscreenVideo={false}
-          allowsInlineMediaPlayback
-          scalesPageToFit={false}
-          onMessage={onMessageRecieved}
-          mediaPlaybackRequiresUserAction={false}
-        />
-      </View>
-      <View style={styles.buttonRow}>
-        <Button title="Play" onPress={playVideo} />
-        <Button title="Pause" onPress={pauseVideo} />
-        <Button title="Restart" onPress={restartVideo} />
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.timeLabel}>{time}</Text>
-      </View>
-    </SafeAreaView>
+    <View>
+      <YoutubePlayer height={300} videoId={'s-l8y4Lcc2Q'} />
+    </View>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  videoContainer: {
-    height: 250,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-around',
-  },
-  buttonRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-  },
-  textContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  timeLabel: {
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-});
-
-export default App;
+}
