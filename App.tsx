69b473e7659a86e25271761c9b452b0fcac74890
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-04-15 20:37:49 +0900

    :technologist: learn: react-native-youtube-iframe

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@
 
 import React, {useState, useCallback, useRef} from 'react';
 import {Button, View, Alert} from 'react-native';
+// import YouTube from 'react-native-youtube';
 import YoutubePlayer from 'react-native-youtube-iframe';
 
 export default function App() {
@@ -18,6 +19,13 @@ export default function App() {
 
   return (
     <View>
+      {/* <YouTube
+        videoId="KVZ-P-ZI6W4" // The YouTube video ID
+        apiKey="YOUR_YOUTUBE_DEVELOPER_API_KEY_FOR_ANDROID"
+        play // control playback of video with true/false
+        fullscreen // control whether the video should play in fullscreen or inline
+        loop // control whether the video should loop when ended
+      /> */}
       <YoutubePlayer height={300} videoId={'s-l8y4Lcc2Q'} />
     </View>
   );
