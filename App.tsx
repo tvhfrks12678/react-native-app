832c58ef96609e9a0bcf7f806ad3936d840e864c
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-04-30 14:59:25 +0900

    :technologist: learn: CollapsibleTabView

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import React, {useState, useCallback, useRef} from 'react';
 import {Button, View, Alert, Text, SafeAreaView} from 'react-native';
 // import YouTube from 'react-native-youtube';
 import YoutubePlayer from 'react-native-youtube-iframe';
+import CollapsibleTabView from './features/learns/collapsibleHeader/CollapsibleTabView';
 import FlatlistSelectable from './features/learns/components/FlatlistSelectable';
 import FlatListSimpleTest from './features/learns/components/FlatListSimpleTest';
 import HeaderScrollableTabView from './features/learns/components/HeaderScrollableTabView';
@@ -16,6 +17,7 @@ import PullRefreshTabView from './features/learns/components/PullRefreshTabView'
 import TabViewSimple from './features/learns/components/TabViewSimple';
 import Page from './features/learns/context/Page';
 import Basics from './features/learns/interpolate/Basics';
+import Example from './features/learns/scrollView/Example';
 import TabViewExample from './features/learns/tabView/TabViewExample';
 
 export default function App() {
@@ -32,10 +34,12 @@ export default function App() {
         {/* <HeaderScrollableTabView /> */}
         {/* <FlatlistSelectable /> */}
         {/* <FlatListSimpleTest /> */}
-        <TabViewSimple />
+        {/* <TabViewSimple /> */}
         {/* <Basics /> */}
         {/* <Page /> */}
         {/* <TabViewExample /> */}
+        {/* <Example /> */}
+        <CollapsibleTabView />
       </SafeAreaView>
     </>
   );
